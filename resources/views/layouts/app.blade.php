43dbfb25b192a66fce713f30aa7488f664c76cc0
restuadkh <restuadimas11@yahoo.com>
2023-12-07 18:06:20 +0700

    edituser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,6 +66,22 @@
                             <a class="nav-link" href="{{ route('pays.index') }}">Pays</a>
                         </li>
                     </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <div class="btn-group">
+                              <button type="button" class="btn btn-secondary">Split dropdown</button>
+                              <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="sr-only">Toggle Dropdown</span>
+                              </button>
+                              <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item disabled" href="#">Disabled action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Separated link</a>
+                              </div>
+                            </div>
+                        </li>
+                    </ul>
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,37 +2,49 @@
 
 @extends('layouts.app')
 
-@section('content') 
-    <div class="col-md">
-        <h2>Edit Pengguna</h2>
-    </div> 
-    <form action="{{ route('users.update', $user->user_id) }}" method="POST">
-        @csrf
-        @method('PUT')
-
-        <!-- Tambahkan input form sesuai dengan atribut yang diperlukan -->
-        <div class="mb-3">
-            <label for="name" class="form-label">Nama</label>
-            <input type="text" class="form-control" id="name" name="name" value="{{ $user->name }}" required>
+@section('content')
+    <div class="container">
+        <div class="col-md">
+            <h2>Edit Pengguna</h2>
         </div>
+        <div class="col-md">
+            <form action="{{ route('users.update', $user->user_id) }}" method="POST">
+                @csrf
+                @method('PUT')
 
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" required>
+                <!-- Tambahkan input form sesuai dengan atribut yang diperlukan -->
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nama</label>
+                    <input type="text" class="form-control" value="{{ $user->name }}" readonly>
+                </div>
+
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" value="{{ $user->email }}" readonly>
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="is_admin" name="is_admin"
+                        {{ $user->is_admin ? 'checked' : '' }}>
+                    <label class="form-check-label" for="is_admin">Admin</label>
+                </div>
+                
+                <div class="mb-3">
+                    <label for="phone_number" class="form-label">Phone Number</label>
+                    <input type="number" class="form-control" id="phone_number" name="phone_number">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+            </form>
         </div>
-
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password">
+        <div class="col-md">
+            <hr>
+            <a href="{{ route('users.index') }}" class="btn btn-secondary">Kembali ke Manajemen Pengguna</a>
         </div>
-
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="is_admin" name="is_admin" {{ $user->is_admin ? 'checked' : '' }}>
-            <label class="form-check-label" for="is_admin">Admin</label>
-        </div>
-
-        <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
-    </form>
-
-    <a href="{{ route('users.index') }}" class="btn btn-secondary">Kembali ke Manajemen Pengguna</a>
+    </div>
 @endsection
